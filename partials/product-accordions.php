ccc412413cac8e97376eff1a2367941629b55ec3
Emir <emir@nit.ba>
2023-04-10 19:37:08 +0200

    debug

--- partials/product-accordions.php
+++ partials/product-accordions.php
@@ -57,6 +57,8 @@ if (have_rows('pim_compatible_modalities')) {
 }
 
 //
+var_dump(have_rows('pim_disclaimers'));
+
 if (have_rows('pim_disclaimers')) {
     var_dump("PIM disclaimers");
     $pim_disclaimers = '';
